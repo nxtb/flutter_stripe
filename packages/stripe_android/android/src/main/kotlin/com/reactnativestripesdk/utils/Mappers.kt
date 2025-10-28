084952ab540657bbb4a228d9c40888e94561e539
Jon Phipps <59151421+phipps980316@users.noreply.github.com>
2025-10-28 09:16:34 +0000

    fix error

--- packages/stripe_android/android/src/main/kotlin/com/reactnativestripesdk/utils/Mappers.kt
+++ packages/stripe_android/android/src/main/kotlin/com/reactnativestripesdk/utils/Mappers.kt
@@ -490,6 +490,7 @@ internal fun mapNextAction(type: NextActionType?, data: NextActionData?): Writab
     NextActionType.BlikAuthorize, NextActionType.UseStripeSdk, null -> {
       return null
     }
+    NextActionType.UpiAwaitNotification -> {}
   }
   return nextActionMap
 }
